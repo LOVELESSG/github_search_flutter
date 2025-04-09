31c3ef3eeb4c8cf7e437d6ef2b819c13c7f4aa89
Li Longzhen <koreabnl@gmail.com>
2025-04-10 01:59:24 +0900

    synchronize

---
+++ lib/repo_detail.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RepoDetail extends StatelessWidget {
+  const RepoDetail({super.key});
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60.0,
+      height: 30.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        //color:
+      ),
+    );
+  }
+}
